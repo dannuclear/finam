17803e8c5850cc645542624f4b659005347de90f
Denis xubuntu <danvaganov@gmail.com>
2026-09-15 20:19:34 +0500

    minus one day

--- src/main/frontend/src/pages/analysis-relative-spreads/index.tsx
+++ src/main/frontend/src/pages/analysis-relative-spreads/index.tsx
@@ -16,11 +16,11 @@ import { useMemo, useState } from "react";
 
 const Index = () => {
     const [timeFrame, setTimeFrame] = useState<TimeFrameConfig>(TIMEFRAMES[7]);
-    const [startTime, setStartTime] = useState<Dayjs>(dayjs().subtract(timeFrame.maxDays+1, "day"))
-    const [endTime, setEndTime] = useState<Dayjs>(dayjs().subtract(1, "day"))
+    const [startTime, setStartTime] = useState<Dayjs>(dayjs().subtract(timeFrame.maxDays, "day"))
+    const [endTime, setEndTime] = useState<Dayjs>(dayjs())
     // const [averageTimeFrame, setAverageTimeFrame] = useState<TimeFrameConfig>(TIMEFRAMES[8]);
     const [averageStartTime, setAverageStartTime] = useState<Dayjs>(dayjs().subtract(TIMEFRAMES[8].maxDays / 2 - 40, "day"))
-    const [averageEndTime, setAverageEndTime] = useState<Dayjs>(dayjs())
+    const [averageEndTime, setAverageEndTime] = useState<Dayjs>(dayjs().subtract(1, "day"))
     const [enabledBars, setEnabledBars] = useState<Record<string, boolean>>({})
     const [zEstimate, setZEstimate] = useState<boolean>(false)
 
